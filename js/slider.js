1d3553dbde389cb364eccc711eaf66ffa17385c3
abid199113 <abid01.cnits@gmail.com>
2024-03-10 18:47:20 +0600

    first commit

---
+++ js/slider.js
@@ -0,0 +1,21 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    freeMode: true,
+
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
+      loop: true,
+
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+
+  });
